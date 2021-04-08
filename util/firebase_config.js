7ac99378cdd0616a4fc46f8582bed7dc67e41ad4
Cytescale <65191711+Cytescale@users.noreply.github.com>
2021-04-08 23:01:47 +0530

    prod commit

--- util/firebase_config.js
+++ util/firebase_config.js
@@ -1,6 +1,6 @@
 const FIREBASE_CONFIG_VAR = {
      apiKey: "AIzaSyAvv6PlmyaV8-5W1sT35Sb44jhWF4mucfs",
-     authDomain: "titan-6969.firebaseapp.com",
+     authDomain: "titan-alpha.vercel.app",
      projectId: "titan-6969",
      storageBucket: "titan-6969.appspot.com",
      messagingSenderId: "956422534816",
