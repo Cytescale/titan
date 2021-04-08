359a2e81b9896ec80e4041127e71f907de384e6a
cytescale <nikhil@cytescale.com>
2021-04-08 22:11:07 +0530

    login commit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import LoginAct from './src/login';
 import SignUpAct from './src/signup';
-import firebaseHelper from './src/util/firebase_helper';
+import firebaseHelper from '../util/firebase_helper';
 
 var firebaseHelp = new firebaseHelper();
 export default function Home() {
