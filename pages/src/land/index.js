66fbb57dc95d43362c3be123fe7dccd3e5addf92
cytescale <nikhil@cytescale.com>
2021-04-21 23:31:14 +0530

    visual commit

--- pages/src/land/index.js
+++ pages/src/land/index.js
@@ -14,31 +14,12 @@ import Cookies from 'universal-cookie';
 import { TwitterPicker,ChromePicker, SwatchesPicker} from 'react-color'
 import _URLS from '../../../util/website_urls';
 
+
 const cookies  = new Cookies();
 const storeHelper = new firestoreHelper(cookies.get('accessToken'));
 
-const renderTooltip = (props,data) => {
-     console.log(data)
-     return(
-     <Tooltip id="button-tooltip" {...props}>
-       Simple tooltip
-     </Tooltip>
-   )};
-
    
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-     <a
-       href=""
-       ref={ref}
-       onClick={(e) => {
-         e.preventDefault();
-         onClick(e);
-       }}
-     >
-       {children}
-     </a>
-   ));
-
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (<a href="" ref={ref} onClick={(e) => { e.preventDefault();onClick(e);}}>{children}</a>));
 
 
 var _ELEMENT_CORE_ARRAY = [];
@@ -329,6 +310,7 @@ export default class LandAct extends React.Component{
                                    if(GOT_ARRAY!==null){
                                         _ELEMENT_CORE_ARRAY = GOT_ARRAY;     
                                    }
+                                   this.forceUpdate();
                               }
                          else{
                               console.log('LAND: Page data load failure');
@@ -572,7 +554,7 @@ export default class LandAct extends React.Component{
      }
      _popover_back_overlay(){
           return(
-               <Popover id="popover-basic"  className='popover_back_class'
+               <Popover id="popover-basic"  className='popover_back_class'    backdropClassName="backdrop"
                >
                     <div className='popover_back_class_main_cont'>
                          <div className='popover_back_class_main_cont_tit'>Background</div>
@@ -1990,6 +1972,9 @@ export default class LandAct extends React.Component{
                     </div>
                     <div className='land_act_head_rght_main_cont'>
                               <div className='land_act_head_rght_feed_butt_cont'>
+                                  <Button variant={'primary'} className='land_act_head_rght_main_save_butt'>Save</Button>
+                              </div>
+                              <div className='land_act_head_rght_feed_butt_cont'>
                                   <FeedbackComp/> 
                               </div>
                               <div className='land_act_head_rght_feed_butt_cont'>
@@ -2009,11 +1994,8 @@ export default class LandAct extends React.Component{
                
                <div className='land_act_main_bdy_cont'>
                     <div className='land_act_main_bdy_left_main'>
-                                        <OverlayTrigger  placement="right" delay={{ show: 50, hide: 200 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Add element</Tooltip>)}>
                                         <button className='land_act_main_bdy_left_add_butt' onClick={()=>{this._set_elem_mod(true)}}>+</button>
-                                        </OverlayTrigger>
-
-                                        <OverlayTrigger placement="right" delay={{ show: 50, hide: 200 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Background</Tooltip>)}>
+                                        <OverlayTrigger placement="right"  rootClose={true} delay={{ show: 200, hide: 100 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Background</Tooltip>)}>
                                              <div>
                                                   <OverlayTrigger trigger="click" placement="right" overlay={this._popover_back_overlay} rootClose={true}>
                                                        <button className='land_act_back_cust_butt'>
@@ -2023,9 +2005,14 @@ export default class LandAct extends React.Component{
                                              </div>
                                         </OverlayTrigger>
 
-                                        <OverlayTrigger placement="right" delay={{ show: 50, hide: 200 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Page settings</Tooltip>)}>
+                                        <OverlayTrigger placement="right"  rootClose={true} delay={{ show: 200, hide:100 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Page settings</Tooltip>)}>
                                         <button className='land_act_back_cust_butt'>
                                              <svg className='land_act_back_cust_butt_ico'  viewBox='0 0 512 512'><title>Hammer</title><path d='M277.42 247a24.68 24.68 0 00-4.08-5.47L255 223.44a21.63 21.63 0 00-6.56-4.57 20.93 20.93 0 00-23.28 4.27c-6.36 6.26-18 17.68-39 38.43C146 301.3 71.43 367.89 37.71 396.29a16 16 0 00-1.09 23.54l39 39.43a16.13 16.13 0 0023.67-.89c29.24-34.37 96.3-109 136-148.23 20.39-20.06 31.82-31.58 38.29-37.94a21.76 21.76 0 003.84-25.2zM478.43 201l-34.31-34a5.44 5.44 0 00-4-1.59 5.59 5.59 0 00-4 1.59h0a11.41 11.41 0 01-9.55 3.27c-4.48-.49-9.25-1.88-12.33-4.86-7-6.86 1.09-20.36-5.07-29a242.88 242.88 0 00-23.08-26.72c-7.06-7-34.81-33.47-81.55-52.53a123.79 123.79 0 00-47-9.24c-26.35 0-46.61 11.76-54 18.51-5.88 5.32-12 13.77-12 13.77a91.29 91.29 0 0110.81-3.2 79.53 79.53 0 0123.28-1.49C241.19 76.8 259.94 84.1 270 92c16.21 13 23.18 30.39 24.27 52.83.8 16.69-15.23 37.76-30.44 54.94a7.85 7.85 0 00.4 10.83l21.24 21.23a8 8 0 0011.14.1c13.93-13.51 31.09-28.47 40.82-34.46s17.58-7.68 21.35-8.09a35.71 35.71 0 0121.3 4.62 13.65 13.65 0 013.08 2.38c6.46 6.56 6.07 17.28-.5 23.74l-2 1.89a5.5 5.5 0 000 7.84l34.31 34a5.5 5.5 0 004 1.58 5.65 5.65 0 004-1.58L478.43 209a5.82 5.82 0 000-8z' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/></svg>
+                                        </button>
+                                        </OverlayTrigger>
+                                        <OverlayTrigger placement="right"  rootClose={true} delay={{ show: 200, hide:100 }} overlay={(props)=>(<Tooltip id="button-tooltip" {...props}>Information</Tooltip>)}>
+                                        <button className='land_act_back_cust_butt'>
+                                             <svg className='land_act_back_cust_butt_ico' viewBox='0 0 512 512'><title>Information Circle</title><path d='M248 64C146.39 64 64 146.39 64 248s82.39 184 184 184 184-82.39 184-184S349.61 64 248 64z' fill='none' stroke='currentColor' stroke-miterlimit='10' stroke-width='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M220 220h32v116'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-miterlimit='10' stroke-width='32' d='M208 340h88'/><path d='M248 130a26 26 0 1026 26 26 26 0 00-26-26z'/></svg>
                                         </button>
                                         </OverlayTrigger>
                     
@@ -2065,27 +2052,9 @@ export default class LandAct extends React.Component{
                          </div>
                          </div>
                     </div>
-                    {/* <div className='land_act_prv_main_cont'>
-                         <div className='land_act_prv_sub_cont'>
-                              <div className='land_act_prv_add_bar_cont'>
-                                        <div className='land_act_prv_add_bar'>
-                                             https://titan.com/page/56197
-                                        </div>
-                                        <Button variant={"light"} className='land_act_prv_add_bar_rel_butt'>Reload</Button>
-                              </div>
-                              <div className='land_act_prv_base_cont' dangerouslySetInnerHTML={{ __html:_PRIVEW_GEN_CODE }}>
-                              </div>
-                         </div>
-                    </div> */}
-               </div>
-               {/* <div className='land_act_creat_butt_main_cont'>
-                    <Button variant={"primary"} onClick={()=>{this._set_elem_mod(true)}}className='land_act_creat_butt'>
-                    add element 
-                    <svg className='land_act_creat_butt_ico' viewBox='0 0 512 512'><title>Add Circle</title><path d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z' fill='none' stroke='currentColor' stroke-miterlimit='10' stroke-width='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M256 176v160M336 256H176'/></svg></Button>
-               </div> */}
+               </div>            
                {this._render_notif()}
-          <div className='app_ver_cont'>Version: {process.env.DEV_VERSION}</div>
-        
+               {/* <div className='app_ver_cont'>Version: {process.env.DEV_VERSION}</div> */}
         </div>
      );
 }
@@ -2193,7 +2162,7 @@ class FeedbackComp extends React.Component{
           return(
                <div className='feed_back_main_cont'>
                <OverlayTrigger trigger="click" rootClose={true} target={this} placement="bottom" overlay={this.feedBackPop()}>
-               <Button variant={'outline-light'}>Feedback</Button>
+               <Button variant={'outline-light'}  className='feed_back_main_butt'>Feedback</Button>
                </OverlayTrigger>     
                </div>
           )
