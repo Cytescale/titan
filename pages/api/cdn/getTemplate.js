d560e951ec51ee788020d341cf7ae82effa80013
cytescale <nikhil@cytescale.com>
2021-05-09 03:17:41 +0530

    commit

--- pages/api/cdn/getTemplate.js
+++ pages/api/cdn/getTemplate.js
@@ -1,27 +1,25 @@
 const fs = require('fs')
-
-
-
-function getFile(url){
-     try {
-          const data = fs.readFileSync(url, 'utf8')
-          return data;
-        } catch (err) {
-          console.error(err);
-          return null;
-        }
-        return null;
-}
-
-
+const admin = require("firebase-admin");
+import('firebase/firebase-storage');
+var bucket = admin.storage().bucket('gs://titan-6969.appspot.com');
 export default async (req, res) => {
      let  temp_code = req.body.temp_code;
      switch(parseInt(temp_code)){
           case 1:{
-               
-               let data  = getFile(__dirname+'/templates/titan-template-1.json');
-               if(data){res.status(200).send({errBool:false,errMess:null,data:data});}
-               else{res.status(200).send({errBool:true,errMess:'File collection error',data:null});}
+                    try{
+                    let data = '';
+                    const strm = bucket.file('titan-template-1.json').createReadStream();
+                    strm.on('data',(d)=>{
+                         data+=d;
+                    }).on('end',()=>{
+                         if(data){res.status(200).send({errBool:false,errMess:null,data:data});}
+                         else{res.status(200).send({errBool:true,errMess:'File collection error',data:null});}         
+                         return;
+                    })}
+                    catch(e){
+                         res.status(200).send({errBool:true,errMess:'File id fault',data:null});          
+                         return;
+                    }
                break;
           }
           default:{
